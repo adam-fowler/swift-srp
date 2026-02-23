d240d743bacbae1182dd911e4827f002ab62332a
Håkon Knutzen <2263015+hakonk@users.noreply.github.com>
2026-02-23 11:11:52 +0100

    Increase salt bytes to 32 from 16 + use platform secure random generator for salt generation (#18)
    * Increase salt size from 16 to 32 bytes
    
    * Improve salt generation with cryptographically secure random using Security framework
    
    * Simplify secure random implementation using direct array reference
    
    * Add clean tests for secure random generation
    
    * Fix salt tests to be more realistic about random distribution
    
    * Remove overly complex salt generation test
    
    * Fix: Use MemoryLayout.size instead of stride for accurate byte count
    
    * use SystemRandomNumberGenerator for generating random UInt8
    
    * move random(count:) to other extension

--- Sources/SRP/Array.swift
+++ Sources/SRP/Array.swift
@@ -1,20 +1,23 @@
-
 extension Array where Element: FixedWidthInteger {
-    /// create array of random bytes
-    static func random(count: Int) -> [Element] {
-        var array = self.init()
-        for _ in 0 ..< count {
-            array.append(.random(in: Element.min ..< Element.max))
-        }
-        return array
-    }
-
     /// generate a hexdigest of the array of bytes
     func hexdigest() -> String {
         return map {
             let characters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f"]
             return "\(characters[Int($0 >> 4)])\(characters[Int($0 & 0xF)])"
         }.joined()
+    }
+
+    /// create array of random bytes using cryptographically secure random number generation
+    static func random(count: Int) -> [Element] {
+        var array = [Element]()
+        array.reserveCapacity(count)
+        var g = SystemRandomNumberGenerator()
+        var i = 0
+        while i < count {
+            array.append(Element.random(in: Element.min...Element.max, using: &g))
+            i += 1
+        }
+        return array
     }
 }
 
